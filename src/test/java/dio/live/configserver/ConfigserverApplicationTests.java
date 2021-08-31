a675c774ff9d77610cf25848021898a94fc64483
rogs <pilique11@gmail.com>
2021-08-31 20:09:12 -0300

    conf server up

---
+++ src/test/java/dio/live/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.live.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
